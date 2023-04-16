8dbe5db385b43e28f04967696ac2328fd1f2f9bc
NastyaKatyushkina <nastyusha_kat@mail.ru>
2023-04-16 23:05:28 +0500

    ajh-hw-12-b

--- server.js
+++ server.js
@@ -1,51 +1,55 @@
-const http = require('http');
-const Koa = require('koa');
-const koaBody = require('koa-body');
-const Router = require('koa-router');
-const cors = require('koa2-cors');
-const faker = require('faker');
-var slow = require('koa-slow');
+const http = require("http")
+const Koa = require("koa")
+const koaBody = require("koa-body")
+const cors = require("@koa/cors")
+const mailDb = require("./db")
+const Router = require("koa-router")
+const koaStatic = require("koa-static")
+const uuid = require("uuid")
+const path = require("path")
+const fs = require("fs")
+const newsDb = require("./db")
 
-const app = new Koa();
-app.use(slow({
-  delay: 5000
-}));
+const app = new Koa()
 
-// Body Parsers
-app.use(koaBody({ json: true, text: true, urlencoded: true }));
+const public = path.join(__dirname, "db/public")
 
-// CORS
+app.use(cors())
+
+app.use(koaStatic(public))
+
 app.use(
-  cors({
-    origin: '*',
-    credentials: true,
-    'Access-Control-Allow-Origin': true,
-    allowMethods: ['GET'],
-  })
-);
+	koaBody({
+		urlencoded: true,
+		json: true
+	})
+)
 
-const router = new Router();
-faker.locale = "ru";
+const router = new Router()
 
-router.get('/news', async (ctx) => {
-  const newsList = [];
-  
-  // FAKER data
-  for (let i = 0; i < 5; i +=1) {
-    const news = {
-      id: faker.datatype.uuid(),
-      received: Date.now(),
-      image: faker.image.avatar(),
-      body: faker.lorem.sentence(),
-    };
-    newsList.push(news);
-  };
+router.get("/news", async ctx => {
+	ctx.response.body = JSON.stringify(newsDb.news)
+})
 
-  ctx.response.body = newsList;
-});
+router.get("/", async ctx => {
+	ctx.response.body = "server D&D.projects start"
+})
 
-app.use(router.routes()).use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods())
 
-const port = process.env.PORT || 7070;
-const server = http.createServer(app.callback());
-server.listen(port, () => console.log('Server started'));
+const server = http.createServer(app.callback())
+
+const port = process.env.PORT || 3000
+
+server.listen(port, err => {
+	if (err) {
+		console.log(err)
+
+		return
+	}
+
+	console.log("Server is listening to " + port)
+})
+
+module.exports = public
+
